d841733eb941aa45a83076577e3f3c5056855f32
Saliya Ekanayake <esaliya@gmail.com>
2016-06-20 15:10:45 -0400

    Adding divide by short.max

--- src/main/java/org/saliya/flinkit/utils/SimpleBinaryWriter.java
+++ src/main/java/org/saliya/flinkit/utils/SimpleBinaryWriter.java
@@ -6,10 +6,11 @@ public class SimpleBinaryWriter {
     public static void main(String[] args) {
         String file = "src/main/resources/sample.bin";
         try (DataOutputStream dos = new DataOutputStream(new FileOutputStream(file))) {
-            int size = 128;
+            int size = 8;
+            int totalItems = size * size;
             for (int i = 0; i < size; ++i){
                 for (int j = 0; j < size; ++j){
-                    dos.writeShort(i*size+j);
+                    dos.writeShort((short)(((i*size+j)*1.0/totalItems)*Short.MAX_VALUE));
                 }
             }
             dos.flush();
